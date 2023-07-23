8827f3c69f226bcb399332eb6f6ca99d6baf6e9f
Min Lwin <lwin.zawmin@gmail.com>
2023-07-23 14:13:06 +0630

    Commit Load On Start Up

--- servlet/01-hello-servlet/src/main/java/com/jdc/demo/HelloServlet.java
+++ servlet/01-hello-servlet/src/main/java/com/jdc/demo/HelloServlet.java
@@ -12,6 +12,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Initialize Hello Servlet");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
