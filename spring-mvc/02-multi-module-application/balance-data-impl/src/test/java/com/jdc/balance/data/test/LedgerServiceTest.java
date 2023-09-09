abcadf7e964f333f848b516ed25cf5db26102e23
Min Lwin <lwin.zawmin@gmail.com>
2023-09-09 17:00:51 +0630

    Add Files for Ledger Creation Test

--- spring-mvc/02-multi-module-application/balance-data-impl/src/test/java/com/jdc/balance/data/test/LedgerServiceTest.java
+++ spring-mvc/02-multi-module-application/balance-data-impl/src/test/java/com/jdc/balance/data/test/LedgerServiceTest.java
@@ -1,5 +1,10 @@
 package com.jdc.balance.data.test;
 
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jdc.balance.model.config.DatabaseConfiguration;
+
+@SpringJUnitConfig(classes = DatabaseConfiguration.class)
 public class LedgerServiceTest {
 
 }
